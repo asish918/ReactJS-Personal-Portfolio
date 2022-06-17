c7030caecb24a08451d2e9f82f279b8ebabf6654
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-17 22:38:15 +0530

    Added assests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { About, Footer, Header, Skills, Testimonials, Work } from './container'
 import { Navbar } from './components'
 
+import './App.scss'
+
 function App() {
   return (
       <div className='app'>
