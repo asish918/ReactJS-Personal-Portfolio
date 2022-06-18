78ebc15f399a29bc960a5428c273458bb2fcd4b6
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-18 12:08:49 +0530

    Connected Sanity Backend

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,14 +2,19 @@ import React, { useState, useEffect  } from "react";
 import './About.scss'
 import { motion } from "framer-motion";
 import { images } from "../../constants";
-
-const abouts = [
-    { title: 'Fontend Development', description: 'Self Appreciation OP', imgUrl: images.about01 },
-    { title: 'Backend  Development', description: 'Self Appreciation OP', imgUrl: images.about02 },
-    { title: 'UI/UX', description: 'Self Appreciation OP', imgUrl: images.about03 },
-]
+import { client, urlFor } from "../../client";
 
 const About = () => {
+    const[abouts, setAbouts] = useState([])
+
+    useEffect(() => {
+        const query = '*[_type == "abouts"]'
+
+        client.fetch(query).then((data) => {
+            setAbouts(data)
+        })
+    }, [])
+    
     return (
         <>
         <h2 className="head-text">
@@ -25,7 +30,7 @@ const About = () => {
                 className="app__profile-item"
                 key={index}
                 >
-                    <img src={about.imgUrl} alt="about-title" />
+                    <img src={urlFor(about.imgUrl)} alt="about-title" />
                     <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
                     <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
                 </motion.div>
