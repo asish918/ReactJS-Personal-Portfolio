7bbe07fd6d5228dfec5ba5a391aa2e61ca5551b8
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-18 11:33:47 +0530

    Styled About component

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect  } from "react";
 import './About.scss'
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
+const abouts = [
+    { title: 'Fontend Development', description: 'Self Appreciation OP', imgUrl: images.about01 },
+    { title: 'Backend  Development', description: 'Self Appreciation OP', imgUrl: images.about02 },
+    { title: 'UI/UX', description: 'Self Appreciation OP', imgUrl: images.about03 },
+]
 
 const About = () => {
     return (
-        <div>
-            About
+        <>
+        <h2 className="head-text">
+            I know that <span>Good Dev </span><br />means <span>Good Business</span>
+        </h2>
+
+        <div className="app__profiles">
+            {abouts.map((about, index) => (
+                <motion.div
+                whileInView={{opacity: 1}}
+                whileHover={{scale: 1.1}}
+                transition={{duration: 0.5, type: 'tween'}}
+                className="app__profile-item"
+                key={index}
+                >
+                    <img src={about.imgUrl} alt="about-title" />
+                    <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
+                    <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
+                </motion.div>
+            ))}
         </div>
+        </>
     )
 }
 
